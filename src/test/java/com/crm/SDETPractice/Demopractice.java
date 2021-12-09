f245148ae2b408ec32a598ab076e5fbb179f5bf8
babu <babu@babu-PC>
2021-12-09 14:17:31 +0530

    test data added

---
+++ src/test/java/com/crm/SDETPractice/Demopractice.java
@@ -0,0 +1,17 @@
+package com.crm.SDETPractice;
+
+import org.testng.annotations.Test;
+
+public class Demopractice {
+	
+		@Test
+		public void demoProgram()
+		{
+			System.out.println("this is user1");
+		}
+
+
+	}
+
+
+
